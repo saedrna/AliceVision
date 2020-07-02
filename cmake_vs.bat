7bc9318677c34a8ccda5ecd291001dec79934564
Han <huhan8807@gmail.com>
2020-07-02 17:39:24 +0800

    build script 1

---
+++ cmake_vs.bat
@@ -0,0 +1,21 @@
+mkdir build_vs
+cd build_vs
+
+cmake -G "Visual Studio 16 2019" -A x64 -T host=x64 ^
+      -DCMAKE_TOOLCHAIN_FILE=%CONDA_PREFIX%\h2o.cmake ^
+      -DCMAKE_INSTALL_PREFIX=./install ^
+      -DTARGET_ARCHITECTURE="nehalem" ^
+      -DALICEVISION_BUILD_DOC=OFF ^
+      -DALICEVISION_USE_POPSIFT=ON ^
+      -DALICEVISION_USE_CUDA=ON ^
+      -DALICEVISION_USE_OPENCV=ON ^
+      -DALICEVISION_USE_ALEMBIC=ON ^
+      -DALICEVISION_REQUIRE_CERES_WITH_SUITESPARSE=OFF ^
+      -DBOOST_ALL_DYN_LINK=1 ^
+      -DBoost_USE_STATIC_LIBS=OFF ^
+      -DFLANN_INCLUDE_DIR_HINTS=%CONDA_PREFIX%\include ^
+      -DLEMON_INCLUDE_DIR_HINTS=%CONDA_PREFIX%\include ^
+      -DCUDA_TOOLKIT_ROOT_DIR="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.2" ^
+      ../
+
+cd..
---
+++ environment.bat
@@ -0,0 +1,3 @@
+pushd "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community"
+call VC\Auxiliary\Build\vcvars64.bat
+popd
